1970d2ac7d1083ac20afb92ab088f5b025531d7e
hind-hb <hind.awosat@gmail.com>
2022-02-02 17:56:34 +0200

    add 13

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS movies;
+    
+   CREATE TABLE IF NOT EXISTS movies
+    (title VARCHAR(255),
+   release_date VARCHAR(255),
+   poster_path VARCHAR(255),
+   overview VARCHAR(255),
+   original_name VARCHAR(255)) ;
