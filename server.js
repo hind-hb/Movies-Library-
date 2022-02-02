00cff821f29324ec39fe1d6363e3cfa9709df492
hind.hbashneh <75991604+hind-hb@users.noreply.github.com>
2022-02-02 23:58:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,9 @@ server.get('/trending',handel);
 server.get('/search',search);
 server.post('/addMovie',AddMovie);
 server.use('/getMovies',GetMovies) 
-
 server.put ('/update/:id',UpdateMovie);
 server.delete('/delete/:id',DeleteMovie);
 server.get('/getMovie/:id',GetMovieId);
-
-
 server.use(handleServerError) 
 server.use('*',notFoundHandler);
 
@@ -82,9 +79,6 @@ function AddMovie (req,res){
 
 }
 
-
-
-
 function GetMovies (request,response)
 {
     let sql='SELECT * FROM movies;';
@@ -94,6 +88,45 @@ function GetMovies (request,response)
         handleServerError(error,request,response);
     });
 }
+
+function UpdateMovie (request,response){
+
+    const id = request.params.id;
+    const movie = request.body; 
+    const sql = `UPDATE movies 
+    SET title=$1,release_date=$2, poster_path=$3, overview=$4,original_name=$5
+    WHERE id = $6 RETURNING *;`;
+    let values = [movie.title,movie.release_date,movie.poster_path,movie.overview,movie.original_name,id]; 
+    client.query (sql,values).then(data=>{
+        response.status(200).json(data.rows);
+    }).catch(error=>{
+        console.log(error);
+        handleServerError(error,request,response);
+    });
+} 
+function DeleteMovie(request,response){
+    const id = request.params.id;
+
+    const sql = `DELETE FROM movies WHERE id=${id};`; 
+    client.query(sql).then(()=>{
+        response.status(200).json("Movie has been deleted");
+    }).catch(error=>{
+        handleServerError(error,request,response);
+    });
+}
+function GetMovieId (request,response){
+    const id = request.params.id;
+
+    const sql = `SELECT * FROM movies WHERE id=$1;`;
+    const values = [id]; 
+    client.query(sql,values).then(data=>{
+        response.status(200).json(data.rows)
+        }).catch(error=>{
+            
+            handleServerError(error,request,response);
+        });
+}
+
 function notFoundHandler(req,res){
     res.status(404).send("This page is not found")
  }
